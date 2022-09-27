5bba61d722cdec7e6d92ace7f5f594b62bdb2524
Andrew Zyuzin <akyc@users.noreply.github.com>
2022-09-27 17:28:09 +0300

    .

--- code.ts
+++ code.ts
@@ -1,18 +1,24 @@
-figma.showUI(__html__);
+figma.showUI(__html__, {"title": 'Конструктор стендов Werkel', 'width': 400, 'height': 600});
 figma.ui.onmessage = msg => {
   if(msg.type === 'copy'){
     let result = {}
-    for (const node of figma.currentPage.selection) {
-      let code = node.name.replace(/^.*\(|\)$/gm, '')
-      if(code in result){
-        result[code]++
-      } else {
-        result[code] = 1
+    if (figma.currentPage.selection.length) {
+      for (const node of figma.currentPage.selection) {
+        let code = node.name.replace(/^.*\(|\)$/gm, '')
+        if(code in result){
+          result[code]++
+        } else {
+          result[code] = 1
+        }
       }
+    } else {
+      result = null
     }
+    
     figma.ui.postMessage({
       type: 'ok',
       value: result,
     })
   }
 };
+
